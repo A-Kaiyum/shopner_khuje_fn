5c19b895bc185ba8cd4986d1d155d940728fbb75
akaiyum <akaiyum.cse@gmail.com>
2021-11-23 16:04:13 +0600

    signle page post crud partially done

--- app/Http/Controllers/Frontend/FrontController.php
+++ app/Http/Controllers/Frontend/FrontController.php
@@ -18,9 +18,18 @@ public function home(){
     }
     public function blog(){
 
-        $posts = Post::orderBy('created_at','DESC')->take(5)->get();
+        $posts = Post::with('category','user')->orderBy('created_at','DESC')->take(5)->get();
+        $firstPost = $posts->splice(0,2);
+        $middlePost = $posts->splice(0,1);
+        $lastPost = $posts->splice(0);
+
+        $footerPosts = Post::with('category','user')->inRandomOrder()->limit(4)->get();
+        $firstFooterPost = $footerPosts->splice(0,1);
+        $middleFooterPost = $footerPosts->splice(0,2);
+        $lastFooterPost = $footerPosts->splice(0);
+
         $recentPosts = Post::with('category','user')->orderBy('created_at','DESC')->paginate(6);
-        return view('frontend.pages.blog',compact(['posts','recentPosts']));
+        return view('frontend.pages.blog',compact(['posts','recentPosts','firstPost','middlePost','lastPost','firstFooterPost','middleFooterPost','lastFooterPost']));
     }
     public function singlePost($slug){
 
--- resources/views/frontend/layouts/_header.blade.php
+++ resources/views/frontend/layouts/_header.blade.php
@@ -1,6 +1,6 @@
 <header class="section page-header">
 <div class="rd-navbar-wrap">
-    <nav class="rd-navbar rd-navbar-classic" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-md-layout="rd-navbar-fixed" data-md-device-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-static" data-lg-device-layout="rd-navbar-static" data-xl-layout="rd-navbar-static" data-xl-device-layout="rd-navbar-static" data-xxl-layout="rd-navbar-static" data-xxl-device-layout="rd-navbar-static" data-lg-stick-up-offset="46px" data-xl-stick-up-offset="46px" data-xxl-stick-up-offset="46px" data-lg-stick-up="true" data-xl-stick-up="true" data-xxl-stick-up="true">
+    <nav class="rd-navbar rd-navbar-classic" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-md-layout="rd-navbar-fixed" data-md-device-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-static" data-lg-device-layout="rd-navbar-static" data-xl-layout="rd-navbar-static" data-xl-device-layout="rd-navbar-static" data-xxl-layout="rd-navbar-static" data-xxl-device-layout="rd-navbar-static" data-lg-stick-up-offset="30px" data-xl-stick-up-offset="30px" data-xxl-stick-up-offset="30px" data-lg-stick-up="true" data-xl-stick-up="true" data-xxl-stick-up="true">
         <div class="rd-navbar-main-outer">
             <div class="rd-navbar-main">
                 <!-- RD Navbar Panel-->
@@ -8,7 +8,7 @@
                     <!-- RD Navbar Toggle-->
                     <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar-nav-wrap"><span></span></button>
                     <!-- RD Navbar Brand-->
-                    <div class="rd-navbar-brand"><a href="index.html"><img class="brand-logo-light" src="{{asset('storage/images/logo-1.png')}}" alt="" width="50" height="50"/></a></div>
+                    <div class="rd-navbar-brand"><a href="{{route('frontSide')}}"><img class="brand-logo-light" src="{{asset('storage/images/logo-1.png')}}" alt="" width="50" height="50"/></a></div>
                 </div>
                 <div class="rd-navbar-main-element">
                     <div class="rd-navbar-nav-wrap">
