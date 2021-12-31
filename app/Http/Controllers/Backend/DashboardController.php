a88f14ae09f7cc7cc9f6d7a4682f09a96842ce79
Abdul Kaiyum <akaiyum.cse@gmail.com>
2021-12-31 13:37:53 +0600

    partial change in dashboard and controller

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers\Backend;
 
+use App\Doctor;
+use App\DonateBlood;
 use App\Http\Controllers\Controller;
+use App\Police;
+use App\Post;
+use App\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index(){
-        return view('backend.pages.dashboard');
+        $totalPost = Post::orderBy('id','DESC')->count();
+        $totalUser = User::orderBy('id','DESC')->count();
+        $totalDonner = DonateBlood::orderBy('id','DESC')->count();
+        $donnerProvided = DonateBlood::orderBy('id','DESC')->count();
+        $doctorProvided = Doctor::orderBy('id','DESC')->count();
+        $policeProvided = Police::orderBy('id','DESC')->count();
+        return view('backend.pages.dashboard',compact(['totalPost','totalUser','totalDonner','donnerProvided','doctorProvided','policeProvided']));
     }
 }
